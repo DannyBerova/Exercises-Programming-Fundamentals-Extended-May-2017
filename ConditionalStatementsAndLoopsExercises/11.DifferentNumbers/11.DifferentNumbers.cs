310f2d5b44be5d14a6fa54aa66da1f5e5a8ccdf0
DannyBerova <danny_jordanova@abv.bg>
2017-05-26 02:44:24 +0300

    11th problem Different Numbers 26.05.2017

--- ConditionalStatementsAndLoopsExercises/11.DifferentNumbers/11.DifferentNumbers.cs
+++ ConditionalStatementsAndLoopsExercises/11.DifferentNumbers/11.DifferentNumbers.cs
@@ -8,6 +8,39 @@ namespace _11.DifferentNumbers
         public static void Main()
         {
 
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (Math.Abs(a - b) < 5)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    for (int j = a; j <= b; j++)
+                    {
+                        for (int k = a; k <= b; k++)
+                        {
+                            for (int l = a; l <= b; l++)
+                            {
+                                for (int m = a; m <= b; m++)
+                                {
+                                    if (j <= i || k <= j || l <= k || m <= l)
+                                    {
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"{i} {j} {k} {l} {m}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
