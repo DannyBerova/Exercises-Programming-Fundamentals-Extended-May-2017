08d351781d02a039cdf9e73b79c5b4f7f86b83b7
DannyBerova <danny_jordanova@abv.bg>
2017-07-18 00:00:34 +0300

    Nested Dictionaries Exercises

---
+++ NestedDictionaries-Exercises/04.Shellbound/04.Shellbound.cs
@@ -0,0 +1,38 @@
+﻿
+namespace _04.Shellbound
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var shellsOfVladi = new Dictionary<string, HashSet<long>>();
+            var regionAndSize = Console.ReadLine();
+
+            while (regionAndSize != "Aggregate")
+            {
+                var ofParts = regionAndSize.Split(' ').ToArray();
+                var region = ofParts[0];
+                var size = long.Parse(ofParts[1]);
+
+                if (!shellsOfVladi.ContainsKey(region))
+                {
+                    shellsOfVladi[region] = new HashSet<long>();
+                }
+                shellsOfVladi[region].Add(size);
+                regionAndSize = Console.ReadLine();
+            }
+
+            foreach (var region in shellsOfVladi)
+            {
+                var sum = region.Value.Sum();
+                var result = sum - sum / region.Value.Count;
+                Console.WriteLine
+                    ($"{region.Key} -> {string.Join(", ", region.Value)} ({result})");
+            }
+        }
+    }
+}
