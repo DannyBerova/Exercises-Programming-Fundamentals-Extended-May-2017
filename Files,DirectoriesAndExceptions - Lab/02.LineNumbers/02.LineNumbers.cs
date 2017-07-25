15656c12b277dcfa9b1017a2397115fc9f2d5dac
DannyBerova <danny_jordanova@abv.bg>
2017-07-25 17:08:47 +0300

    Files, Directories and Exceptions - Lab

---
+++ Files,DirectoriesAndExceptions - Lab/02.LineNumbers/02.LineNumbers.cs
@@ -0,0 +1,23 @@
+﻿
+namespace _02.LineNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] textLines = File.ReadAllLines("Input.txt");
+
+            for (int i = 1; i <= textLines.Length; i++)
+            {
+                textLines[i - 1] = i + ". " + textLines[i - 1];
+            }
+
+            File.WriteAllLines("LineNumbers.txt", textLines);
+        }
+    }
+}
---
+++ Files,DirectoriesAndExceptions - Lab/04.MergeFiles/04.MergeFiles.cs
@@ -0,0 +1,25 @@
+﻿
+namespace _04.MergeFiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] fileOne = File.ReadAllLines("FileOne.txt");
+            string[] fileTwo = File.ReadAllLines("FileTwo.txt");
+
+            string[] mergedLists = fileOne
+                .Concat(fileTwo)
+                .OrderBy(x => x)
+                .ToArray();
+
+            File.AppendAllText("MergeFiles.txt",
+                string.Join(Environment.NewLine, mergedLists));
+        }
+    }
+}
