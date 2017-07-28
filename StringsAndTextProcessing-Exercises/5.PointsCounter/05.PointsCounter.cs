5f81e6bef3a6fc0cfe2e9d27fffe1c96e53f9030
DannyBerova <danny_jordanova@abv.bg>
2017-07-28 23:31:45 +0300

    Strings - Exercises - 05.Point Counter finished 100/100

--- StringsAndTextProcessing-Exercises/5.PointsCounter/05.PointsCounter.cs
+++ StringsAndTextProcessing-Exercises/5.PointsCounter/05.PointsCounter.cs
@@ -9,6 +9,53 @@ namespace _5.PointsCounter
     {
         public static void Main()
         {
+            string inputData = Console.ReadLine();
+            var teams = new Dictionary<string, Dictionary<string, int>>();
+            string teamName = string.Empty;
+            string playerName = string.Empty;
+
+
+            while (inputData != "Result")
+            {
+                inputData = inputData
+                    .Replace("@", string.Empty)
+                    .Replace("%", string.Empty)
+                    .Replace("$", string.Empty)
+                    .Replace("*", string.Empty);
+
+                string[] tokens = inputData.Split('|');
+
+                if (tokens[0].Last() > 96)
+                {
+                    teamName = tokens[1];
+                    playerName = tokens[0];
+                }
+                else
+                {
+                    teamName = tokens[0];
+                    playerName = tokens[1];
+                }
+                int points = int.Parse(tokens[2]);
+
+                if (!teams.ContainsKey(teamName))
+                {
+                    teams.Add(teamName, new Dictionary<string, int>());
+                }
+
+                teams[teamName][playerName] = points;
+
+                inputData = Console.ReadLine();
+            }
+
+            foreach (var team in teams.OrderByDescending(x => x.Value.Values.Sum()))
+            {
+                Console.WriteLine($"{team.Key} => {team.Value.Values.Sum()}");
+
+                foreach (var player in team.Value.OrderByDescending(x => x.Value).Take(1))
+                {
+                    Console.WriteLine($"Most points scored by {player.Key}");
+                }
+            }
         }
     }
 }
