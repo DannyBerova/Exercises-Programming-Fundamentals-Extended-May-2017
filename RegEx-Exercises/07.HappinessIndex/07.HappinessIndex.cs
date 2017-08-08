ede22e7c775eacabbf4abcc7d04b2d406211ed57
DannyBerova <danny_jordanova@abv.bg>
2017-08-09 01:44:35 +0300

    Final 100/100 RegEx - Exercises (HappinessIndex and Commits)

--- RegEx-Exercises/07.HappinessIndex/07.HappinessIndex.cs
+++ RegEx-Exercises/07.HappinessIndex/07.HappinessIndex.cs
@@ -11,6 +11,41 @@ namespace _07.HappinessIndex
     {
         public static void Main()
         {
+            string happyPattern = @"(:\)|:D|;\)|:\*|:]|;]|:}|;}|\(:|\*:|c:|\[:|\[;)";
+            string sadPattern = @"(:\(|D:|;\(|:\[|;\[|:{|;{|\):|:c|\]:|\];)";
+
+            double happinessIndex = 0.0;
+
+            string input = Console.ReadLine();
+
+            var happyEmoticons = Regex.Matches(input, happyPattern);
+            var sadEmoticons = Regex.Matches(input, sadPattern);
+
+            happinessIndex = (double)happyEmoticons.Count / sadEmoticons.Count;
+
+            string status = string.Empty;
+
+            if (happinessIndex >= 2)
+            {
+                status = ":D";
+            }
+            else if (happinessIndex > 1)
+            {
+                status = ":)";
+            }
+            else if (happinessIndex == 1)
+            {
+                status = ":|";
+            }
+            else if (happinessIndex < 1)
+            {
+                status = ":(";
+            }
+
+            Console.WriteLine
+                ($"Happiness index: {happinessIndex:F2} {status}");
+            Console.WriteLine(
+                $"[Happy count: {happyEmoticons.Count}, Sad count: {sadEmoticons.Count}]");
         }
     }
 }
